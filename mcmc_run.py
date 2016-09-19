9979112127e86fbc439bd20e0071d11f30fb68f9
Olegs <Oleg@MacBook-Pro-de-Oleg.local>
2016-09-18 19:49:57 -0500

    better definitions

--- mcmc_run.py
+++ mcmc_run.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import emcee
 import corner
 import numpy as np
@@ -14,7 +15,7 @@ freqs, temp, err = np.loadtxt('dataset7.txt', unpack = True)
 #                           T         nu     sigma     c0       c1      c2     c3
 priors_gauss = np.array([0.,1000., 40.,100., 0.,40., 0.,10., -10.,0., -1.,1.])
 #stuff needed for the mcmc
-nwalkers, b_steps, steps = 300, 1600, 800
+nwalkers, b_steps, steps = 200, 100, 1000
 
 #prior list for the tanh model
 
@@ -22,11 +23,16 @@ priors_tanh = np.array([0.,1., 0.,20., 0.,5, 1000.,10000., 0.,20., 0.,5., 0.,25.
                         0., 10, -10.,0., -1.,1.])
 
 #finding the midpoint of each prior range
-dim = np.size(priors_tanh)/2
-mid = np.zeros(dim)
+ndim = int(np.size(priors_tanh)/2)
+mid = np.zeros(ndim)
 for i in range(ndim):
-    mid[i] = priors_gauss[2*i] + priors_gauss[2*i + 1]
+    mid[i] = priors_tanh[2*i] + priors_tanh[2*i + 1]
 mid = 0.5 * mid
+
+#maximazing the likelihood
+fun = lambda *args: -lnhood(*args)
+result = op.minimize(fun, mid, args = (model, temp, freqs, err), method = 'L-BFGS-B')
+mu = result.x[9:]
 
 #disperse the walkers around the midpoint for each parameter
 z = np.zeros((ndim, nwalkers))
@@ -36,7 +42,7 @@ for k in range(ndim):
     z[k,:] = mid[k] + h1 * np.random.rand(nwalkers)
 
 for i in range(nwalkers):
-    pos1.append(np.array([z[0,i],z[1,i],z[2,i],z[3,i],z[4,i],z[5,i]]))
+    pos1.append(np.array([z[0,i],z[1,i],z[2,i],z[3,i],z[4,i],z[5,i],z[6,i],z[7,i],z[8,i],z[9,i],z[10,i],z[11,i]]))
 
 
 #for i in range(nwalkers):
@@ -47,28 +53,20 @@ for i in range(nwalkers):
 
 
 #burn_in phase
-sampler = emcee.EnsembleSampler(nwalkers, ndim, result, args=[model, temp, freqs, err, priors_gauss])
+print('this is just a test')
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(model, temp, freqs, err, priors_tanh, mu))
 pos, prob, state = sampler.run_mcmc(pos1, b_steps)
-
-#the proper run
-max_prob = np.where(prob==np.max(prob))[0][0]
-new_pos = pos[max_prob]
-y = np.zeros((ndim, nwalkers))
-h2 = 1e-2
-pos2 = []
-for k in range(ndim):
-    y[k,:] = new_pos[k] + h2 * np.random.normal(loc=0.0, scale=np.abs(new_pos[k]), size=nwalkers)
-
-for i in range(nwalkers):
-    pos2.append(np.array([z[0,i],z[1,i],z[2,i],z[3,i],z[4,i],z[5,i]]))
-
+print('the test has ended')
+"""
 sampler.reset()
-sampler.run_mcmc(pos2, steps)
+
+sampler.run_mcmc(pos, steps,rstate0=state)
 print sampler.acceptance_fraction.mean()
 
 #save the chain.
-sample = sampler.chain[:,steps*.5:,:].reshape((-1,ndim))
-np.savetxt('synt1.txt', sample)
+sample = sampler.chai.reshape((-1,ndim))
+np.savetxt('test_normal.txt', sample)
 
 fig = corner.corner(sample)
-fig.savefig('fit.pdf')
+fig.savefig('fit_test.pdf')
+"""
